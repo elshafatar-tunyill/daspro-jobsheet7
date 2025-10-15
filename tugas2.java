382dadd8ccc51baf942c225443df5606bcbe2986
elshafatar-tunyill <elshafatar@gmail.com>
2025-10-15 14:43:39 +0700

    tugas1

---
+++ tugas2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class tugas2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jenis, durasi, total=0;
+        do{
+            System.out.println("Masukan Jenis Kendaraan (1.Mobil = Rp.3000 2.Motor = Rp.2000 0 = keluar)");
+            jenis = sc.nextInt();
+            if (jenis == 1 || jenis == 2) {
+                System.out.println("Masukan Durasi Parkir ");
+                durasi = sc.nextInt();
+            if (durasi > 5) {
+                total += 12500;
+            } else if (jenis == 1) {
+                total += durasi * 3000;
+            } else if (jenis == 2) {
+                total += durasi * 2000;
+            }
+            }               
+        } while (jenis != 0);
+        System.out.println("Total Pendapatan Parkir: Rp." + total);
+        sc.close();
+    }
+}
+
